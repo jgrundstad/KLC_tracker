ed294d10170d9c65b7d69258a1eb2e535addb308
Jason Grundstad <jgrundstad@gmail.com>
2015-02-02 21:27:02 -0600

    added first templates, select procds, show items

--- klc/tracker/views.py
+++ klc/tracker/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from tracker.models import Proceeding, Client, Item, Code
 
 # Create your views here.
+def index(request):
+  proceeding_list = Proceeding.objects.order_by('start_date')
+  context = {'proceeding_list': proceeding_list}
+  return render(request, 'tracker/index.html', context)
+
+
+def item(request, item_id):
+  return HttpResponse("Inspecting item %s." % item_id)
+
+
+def contact(request, contact_id):
+  return HttpResponse("Inspecting contact %s." % contact_id)
+
+
+def proceeding(request, proceeding_id):
+  item_list = Item.objects.filter(proceeding=proceeding_id).order_by('-date')
+  print "looking for proceeding_id: %s" % proceeding_id
+  p_name = Proceeding.objects.filter(id=proceeding_id)[0].name
+  context = {'item_list': item_list, 'proceeding': p_name}
+  return render(request, 'tracker/proceeding.html', context)
+
