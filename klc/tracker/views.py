dfc0d8799144fe1be154a6499485defeaadae304
Jason Grundstad <jgrundstad@gmail.com>
2015-02-08 20:46:29 -0600

    working table for proceeding view

--- klc/tracker/views.py
+++ klc/tracker/views.py
@@ -18,10 +18,10 @@ def contact(request, contact_id):
   return HttpResponse("Inspecting contact %s." % contact_id)
 
 
-def proceeding(request, proceeding_id):
+def proceedings(request, proceeding_id):
   item_list = Item.objects.filter(proceeding=proceeding_id).order_by('-date')
   print "looking for proceeding_id: %s" % proceeding_id
   p_name = Proceeding.objects.filter(id=proceeding_id)[0].name
   context = {'item_list': item_list, 'proceeding': p_name}
-  return render(request, 'tracker/proceeding.html', context)
+  return render(request, 'tracker/proceedings.html', context)
 
