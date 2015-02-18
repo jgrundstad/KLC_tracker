f0d1ca9417933be821d7fb490cca5e741b7025dd
Jason Grundstad <jgrundstad@gmail.com>
2015-02-18 12:45:44 -0600

    added index.html, items.html, and urls.py

---
+++ klc/tracker/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+
+from tracker import views
+
+urlpatterns = patterns('', 
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<item_id>\d+)/item/$', views.item, name='item'),
+    url(r'^(?P<contact_id>\d+)/contact/$', views.contact, name='contact'),
+    url(r'^(?P<proceeding_id>\d+)/items/$', 
+        views.items, name='items'),
+  )
